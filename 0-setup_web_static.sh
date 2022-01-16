eeae367f4754eba5d9c251f1ffdec1a8c27a13bc
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-16 20:26:09 +0300

    added guidelines

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/env bash
 # sets up my web servers for the deployment of web_static
 
-#--updating the packages
+echo -e "\e[1;32m START\e[0m"
+
+#--Updating the packages
 sudo apt-get -y update
 sudo apt-get -y install nginx
+echo -e "\e[1;32m Packages updated\e[0m"
+echo
 
 #--configure firewall
 sudo ufw allow 'Nginx HTTP'
+echo -e "\e[1;32m NGINX firewall configured\e[0m"
+echo
 
 #--created the dir
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo -e "\e[1;32m directories created"
+echo
 
 #--adds test string
 echo "<h1>Welcome to www.beta-scribbles.tech</h1>" > /data/web_static/releases/test/index.html
+echo -e "\e[1;32m Test string added\e[0m"
+echo
 
 #--prevent overwrite
 if [ -d "/data/web_static/current" ];
@@ -20,6 +30,8 @@ then
     echo "path /data/web_static/current exists"
     sudo rm -rf /data/web_static/current;
 fi;
+echo -e "\e[1;32m prevent overwrite\e[0m"
+echo
 
 #--create symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -27,6 +39,9 @@ sudo chown -hR ubuntu:ubuntu /data
 #sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t\tautoindex on;\n\t}" /etc/nginx/sites-available/default
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+echo -e "\e[1;32m Symbolic link created\e[0m"
+echo
 
 #--restart NGINX
 sudo service nginx restart
+echo -e "\e[1;32m restart NGINX\e[0m"
