b14ad05bbc8cf7a1a18a096476b17a46b2b65c17
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-30 18:17:28 +0300

    task 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -47,11 +47,11 @@ def number_n(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('5-number.html', number=n)
+    return render_template('5-number.html', n=n)
 
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def number_odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', number=n)
+    return render_template('6-number_odd_or_even.html', n=n)
 
 
 if __name__ == "__main__":
