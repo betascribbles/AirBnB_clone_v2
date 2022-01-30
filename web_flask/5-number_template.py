6b9e21b575297673c4a24dd93c5369eb17eaa0cd
Firdaus Salim <firdaushassan033@gmail.com>
2022-01-30 18:10:03 +0300

    task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,7 +47,7 @@ def number_n(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template ('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == "__main__":
